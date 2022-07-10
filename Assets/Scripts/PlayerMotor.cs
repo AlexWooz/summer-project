41666c58da648289ba034d6aae94c1c298cb6330
unknown <elouan112@gmail.com>
2022-07-10 19:50:31 -0400

    air drag added

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,16 +6,19 @@ public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
+    private float playerSpeed = 0f;
     private bool isGrounded;
     public float speed = 5f;
     public float sprintMult = 2f;
     public float gravity = 9.8f;
+    public float drag = 0.999f;
     public float jumpHeight = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        playerSpeed = speed;
     }
 
     // Update is called once per frame
@@ -30,10 +33,14 @@ public class PlayerMotor : MonoBehaviour
         Vector3 moveDirection = Vector3.zero;
         moveDirection.x = input.x;
         moveDirection.z = input.y;
-        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        controller.Move(transform.TransformDirection(moveDirection) * playerSpeed * Time.deltaTime);
         playerVelocity.y += -gravity * Time.deltaTime;
-        if (isGrounded && playerVelocity.y < 0) 
+        if (isGrounded && playerVelocity.y < 0) {
+            playerSpeed = speed; 
             playerVelocity.y = -2f;
+        } else if(!isGrounded) {
+            playerSpeed *= drag;
+        }        
         controller.Move(playerVelocity * Time.deltaTime);
         Debug.Log("Player velocity: " + playerVelocity.y);
     }
